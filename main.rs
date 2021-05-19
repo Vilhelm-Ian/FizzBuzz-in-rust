6d7b3f814bff632d769e70cd4cfe321e3b450e69
Vilhelm-Ian <81630959+Vilhelm-Ian@users.noreply.github.com>
2021-05-19 07:48:12 +0000

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,31 @@
+struct Divisor {
+    number: i32,
+    message: String,
+}
+
+impl Divisor {
+    fn new(number: i32, message: String) -> Divisor {
+        Divisor { number, message }
+    }
+}
+
+fn main() {
+    let vec_of_divisors = [
+        Divisor::new(3, String::from("Fizz")),
+        Divisor::new(5, String::from("Buzz")),
+    ];
+
+    for i in 1..100 {
+        let mut result = String::new();
+        for divisor in vec_of_divisors.iter() {
+            if i % divisor.number == 0 {
+                result.push_str(&divisor.message);
+            }
+        }
+        if result == String::new() {
+            println!("{:?}", i);
+            continue;
+        }
+        println!("{:?}", result);
+    }
+}
